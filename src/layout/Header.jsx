88b0e9f86ec35f88f65d656fce4cfc80e107794e
Faßbender Harri <hf020@hdm-stuttgart.de>
2022-11-20 14:16:23 +0100

    added media card & burger menu components

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,14 +8,24 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
-import {Avatar, Link} from "@mui/material";
-import {useProfile} from "../utils/providers/ProfileProvider";
-import {useNavigate} from "react-router-dom";
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Divider from '@mui/material/Divider';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { Avatar, Link } from "@mui/material";
+import { useProfile } from "../utils/providers/ProfileProvider";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+
+    // dummy data for drawer navigation links
+    const navigationLinks = ["Top Articles", "Tech News", "Sports"]
+
     const [anchorEl, setAnchorEl] = useState(null);
-    const {user, logout, isAuthenticated} = useProfile();
+    const { user, logout, isAuthenticated } = useProfile();
     const navigate = useNavigate();
+    const [open, setOpen] = useState(false)
 
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
@@ -25,7 +35,7 @@ export default function Header() {
         setAnchorEl(null);
     };
     return (
-        <Box sx={{flexGrow: 1}}>
+        <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton
@@ -33,12 +43,13 @@ export default function Header() {
                         edge="start"
                         color="inherit"
                         aria-label="menu"
-                        sx={{mr: 2}}
+                        onClick={() => setOpen(true)}
+                        sx={{ mr: 2 }}
                     >
-                        <MenuIcon/>
+                        <MenuIcon />
                     </IconButton>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        <Link to="/" color="inherit" sx={{textDecoration: "none"}}>AppName</Link>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        <Link to="/" color="inherit" sx={{ textDecoration: "none" }}>Newsify</Link>
                     </Typography>
                     <div>
                         <IconButton
@@ -50,12 +61,12 @@ export default function Header() {
                             color="inherit"
                         >
                             {user ?
-                                user.avatar ? <Avatar src={user.avatar}/>
+                                user.avatar ? <Avatar src={user.avatar} />
                                     : <Avatar alt={"Avatar"}>
                                         {user && user.first_name?.charAt(0)}
                                         {user && user.last_name?.charAt(0)}
                                     </Avatar>
-                                : <Avatar/>}
+                                : <Avatar />}
                         </IconButton>
                         <Menu
                             id="menu-appbar"
@@ -77,7 +88,7 @@ export default function Header() {
                                     navigate("user");
                                 }
                                 }>Profile</MenuItem>,
-                                    <MenuItem key="logout" onClick={logout}>Logout</MenuItem>]
+                                <MenuItem key="logout" onClick={logout}>Logout</MenuItem>]
                                 :
                                 <MenuItem key="login" onClick={() => {
                                     navigate("login")
@@ -86,6 +97,27 @@ export default function Header() {
                         </Menu>
                     </div>
                 </Toolbar>
+                <SwipeableDrawer open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
+                    <Toolbar sx={{ minWidth: 250, maxWidth: 'md', display: "flex", justifyContent: "flex-end" }}>
+                        <IconButton
+                            size="large"
+                            color="inherit">
+                            <ChevronLeftIcon onClick={() => setOpen(false)} />
+                        </IconButton>
+                    </Toolbar>
+                    <Divider />
+                    <List>
+                        {
+                            navigationLinks.map(link => (
+                                <ListItem>
+                                    <Typography variant="button" display="block" gutterBottom>
+                                        <Link key={link.id} to={'#'} color="inherit" sx={{ textDecoration: "none" }}>{link}</Link>
+                                    </Typography>
+                                </ListItem>
+                            ))
+                        }
+                    </List>
+                </SwipeableDrawer>
             </AppBar>
         </Box>
     );
