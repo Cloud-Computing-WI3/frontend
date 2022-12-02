4451cc60c49f9a5678058562ef78e0448b911ee6
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2022-12-02 13:23:12 +0100

    added views for different categories

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,58 +2,17 @@ import { Link, Typography, Grid } from "@mui/material";
 import MediaCard from "../components/MediaCard";
 import {useLoaderData} from "react-router-dom";
 
-// static test data
-const dummyData = [
-    {
-        author: "A Destabilizing Hack-and-Leak Operation Hits Moldova",
-        description: "Plus: Google's location snooping ends in a $391 million settlement, Russian code sneaks into US government apps, and the World Cup apps set off alarms.",
-        urlToImage: "https://media.wired.com/photos/637823b284f5d2572f8d0ad0/191:100/w_1280,c_limit/Moldova-Election-Hack-Security-1401716620.jpg",
-        alt: "",
-        primaryLinkTxt: "Read Article",
-        secondaryLinkTxt: "",
-        primaryLink: "#",
-        secondaryLink: "#"
-    },
-    {
-        author: "Samsung Galaxy Watch 5 Pro vs. Apple Watch Ultra: Which flagship watch is best?",
-        description: "The Apple Watch Ultra is far more of a \"pro\" device for athletes and outdoorsmen than the Galaxy Watch 5 Pro, but it's extreme price makes it harder to recommend.",
-        urlToImage: "https://cdn.mos.cms.futurecdn.net/9QSzDsikrSZhbkh6KrFgbg-1200-80.jpg",
-        alt: "",
-        primaryLinkTxt: "Read Article",
-        secondaryLinkTxt: "",
-        primaryLink: "#",
-        secondaryLink: "#",
-    },
-    {
-        author: "I too quit Twitter this week – and have never been more relieved | Bidisha Mamata",
-        description: "Elon Musk’s latest acquisition once turned an innocuous trip to the cinema into something more disturbingLast month, Elon Musk walked into Twitter and then all the good people left. I quit, too. It was a time-sink, a brain drain and an eye strain.In 12 years,…",
-        urlToImage: "https://i.guim.co.uk/img/media/ffcc872456ef4fa4ee569e9123ed41a4ec3895d5/0_155_4000_2399/master/4000.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdG8tb3BpbmlvbnMucG5n&enable=upscale&s=103004481b213ec9f9342ed5386464a5",
-        alt: "",
-        primaryLinkTxt: "Read Article",
-        secondaryLinkTxt: "",
-        primaryLink: "#",
-        secondaryLink: "#"
-    }
-]
-
 export default function HomePage() {
     const data = useLoaderData();
     console.log(data.articles);
     return (
         <>
-            <Link to="user">Go to user</Link>
             <Typography variant="h1" sx={{ m: 2 }}>Top Articles</Typography>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
                 {data.articles && data.articles.map(article => (
                     <Grid item xs={12} sm={6} md={3} key={article.publishedAt}>
                         <MediaCard key={data.id} {...article}></MediaCard>
                     </Grid>))
-                }
-                {
-                    dummyData.map(data =>
-                        <Grid item xs={12} sm={6} md={3} key={data.author}>
-                            <MediaCard key={data.id} {...data}></MediaCard>
-                        </Grid>)
                 }
             </Grid>
         </>
