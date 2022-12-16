23199c430743d26766c9bb6ae62c4733fcb692e5
Danial Eshete <47521900+danialeshete@users.noreply.github.com>
2022-12-16 03:59:17 +0100

    changed the link view

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -17,19 +17,19 @@ export default function MediaCard(props) {
             />
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                    {props.title !== "" && props.title !== null ? props.title : props.category.name !== "" && props.category.name !== null ? props.category.name : "Headline"}
+                    {props.title ? props.title : props.author !== "" && props.author !== null ? props.author : "Headline"}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     {props.description}
                 </Typography>
             </CardContent>
             <CardActions>
-                
+                <a href={props.url} target="_blank" rel="noreferrer">
                 <Button
-                    component={Link}
-                    href={props.primaryLink ? props.primaryLink : props.url}
+                    
                     size="small">{props.primaryLinkTxt ? props.primaryLinkTxt : "Read Article"}
                 </Button>
+                </a>
                 <Button
                     component={Link}
                     href={props.secondaryLink ? props.secondaryLink : props.url}
@@ -39,3 +39,6 @@ export default function MediaCard(props) {
         </Card>
     );
 }
+
+ 
+ 
