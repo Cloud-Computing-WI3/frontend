25a22149fcc0afd75362cd82b7f2636ab1f2e58e
Faßbender Harri <hf020@hdm-stuttgart.de>
2022-12-16 19:30:29 +0100

    added bypass cache param

--- src/utils/apis/news_feed/articles.js
+++ src/utils/apis/news_feed/articles.js
@@ -1,7 +1,14 @@
 import {NewsFeed, responseBody} from "../NewsFeed.jsx";
 
+// to avoid getting cached results set BYPASS_CACHE to true
+const BYPASS_CACHE = false
+const cache_param = BYPASS_CACHE ? `&bypass_cache=true` : `&bypass_cache=false`
+
 const requests = {
-    get: (requestObject) => NewsFeed.get( requestObject.elastic_pointer == null ? `/articles?category_name=${requestObject.category_name}`: `/articles?category_name=${requestObject.category_name}&elastic_pointer=${requestObject.elastic_pointer}`).then(responseBody),
+    get: (requestObject) => NewsFeed.get(requestObject.elastic_pointer == null ?
+        `/articles?category_name=${requestObject.category_name}` + cache_param:
+        `/articles?category_name=${requestObject.category_name}&elastic_pointer=${requestObject.elastic_pointer}` + cache_param
+    ).then(responseBody),
 };
 
 export const Articles = {
