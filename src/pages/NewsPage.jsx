115ecc590ff005257925cbe4c99244d008001674
Danial Eshete <47521900+danialeshete@users.noreply.github.com>
2022-12-16 13:39:49 +0100

    undo pagination

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -2,14 +2,11 @@ import { Typography, Grid } from "@mui/material";
 import MediaCard from "../components/MediaCard";
 import {useLoaderData, useParams} from "react-router-dom";
 import {useState, useEffect, useRef} from "react";
-import Pagination from "react-pagination-js";
 
 export default function NewsPage() {
     const data = useLoaderData();
     const params = useParams();
-    const [headline, setHeadline] = useState(undefined);
-    const [page, setPage] = useState(1);
-    const itemsPerPage = 20;
+    const [headline, setHeadline] = useState(undefined)
 
     useEffect(() => {
     if (params.categoryName) {
@@ -20,29 +17,16 @@ export default function NewsPage() {
         setHeadline(`My categories (${params.categories})`)
     }});
 
-    const handlePageChange = (pageNumber) => {
-        setPage(pageNumber);
-    }
-
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = page * itemsPerPage;
-
     return (
         <>
             <Typography variant="h1" sx={{ m: 2 }}>{headline}</Typography>
             <Grid key={headline} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                {data.articles && data.articles.slice(startIndex, endIndex).map(article => (
+                {data.articles && data.articles.map(article => (
                     <Grid item xs={12} sm={6} md={3} key={article.publishedAt}>
                         <MediaCard key={data.id} {...article}></MediaCard>
                     </Grid>))
                 }
             </Grid>
-            <Pagination
-                currentPage={page}
-                totalSize={data.articles.length}
-                sizePerPage={itemsPerPage}
-                changeCurrentPage={handlePageChange}
-            />
         </>
     )
 }
