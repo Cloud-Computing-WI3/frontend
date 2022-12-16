48e05675553112b3d46cdc4a08b6693e595405f3
Faßbender Harri <hf020@hdm-stuttgart.de>
2022-12-16 18:25:34 +0100

    Finalized infinite scrolling via react InfiniteScroll and included loading icon

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect, useRef } from "react";
 import { Articles } from "../utils/apis/news_feed/articles.js";
 import { ArticlesByKeywords } from "../utils/apis/news_feed/articles_by_keywords.js";
 import { ArticlesByCategories } from "../utils/apis/news_feed/articles_by_categories.js";
+import { ThreeDots } from  'react-loader-spinner'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function NewsPage() {
     const data = useLoaderData();
@@ -12,82 +14,81 @@ export default function NewsPage() {
     const [headline, setHeadline] = useState(undefined)
     const [articles, setArticles] = useState(data.articles || []);
     const [new_elastic_pointer, setNewElasticPointer] = useState(data.elastic_pointer || null);
-    const bottomRef = useRef(null);
+    const [page, setPage] = useState(1)
 
     useEffect(() => {
         if (params.categoryName) {
             setHeadline(params.categoryName[0].toUpperCase() + params.categoryName.substring(1))
+            setArticles(data.articles)
+            setPage(1)
         } else if (params.keywords) {
             setHeadline(`My keywords (${params.keywords})`)
+            setArticles(data.articles)
+            setPage(1)
         } else if (params.categories) {
             setHeadline(`My categories (${params.categories})`)
+            setArticles(data.articles)
+            setPage(1)
         }
-    });
+    }, [params]);
 
-    useEffect(() => {
-        const observer = new IntersectionObserver(([entry]) => {
-            if (entry.isIntersecting) {
-                requestMoreArticles();
-                console.log("new_elastic_pointer: " + new_elastic_pointer)
-            }
-        });
-        observer.observe(bottomRef.current);
-
-        return () => observer.disconnect();
-    }, []);
-
-      
-
-    const requestMoreArticles = () => {
-        //load the articles after the first data.elastic_pointer
-        //if there is no elastic_pointer, then there are no more articles to load
-        if (params.categoryName) {
+    const loadMore = () => {
+         if (params.categoryName) {
             Articles.get({ category_name: params.categoryName, elastic_pointer: new_elastic_pointer }).then(res => {
-                console.log(res)
                 setArticles([...articles, ...res.articles]);
-                
-                setNewElasticPointer(res.elastic_pointer);     
-                           
-
+                setNewElasticPointer(res.elastic_pointer);
+                setPage(page+1)
             }).catch(e => {
-                console.log(e);
+                console.error(e);
             })
         } else if (params.keywords) {
             ArticlesByKeywords.get({ keywords: params.keywords, elastic_pointer: new_elastic_pointer }).then(res => {
-                console.log(res)
                 setArticles([...articles, ...res.articles]);
                 setNewElasticPointer(res.elastic_pointer);
-                console.log("new_elastic_pointer: " + new_elastic_pointer)
-                console.log("resElasticPointer: " + res.elastic_pointer)   
-                console.log("data.elastic_pointer: " + data.elastic_pointer)
-
+                setPage(page+1)
             }).catch(e => {
-                console.log(e);
+                console.error(e);
             })
         } else if (params.categories) {
             ArticlesByCategories.get({ categories: params.categories, elastic_pointer: new_elastic_pointer }).then(res => {
                 console.log(res)
                 setArticles([...articles, ...res.articles]);
                 setNewElasticPointer(res.elastic_pointer);
-
+                setPage(page+1)
             }).catch(e => {
-                console.log(e);
+                console.error(e);
             })
-
         }
     };
 
     return (
         <>
             <Typography variant="h1" sx={{ m: 2 }}>{headline}</Typography>
-            <Grid key={headline} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                {articles.map(article => (
-                    <Grid item xs={12} sm={6} md={3} key={article.publishedAt}>
-                        <MediaCard key={data.id} {...article}></MediaCard>
-                    </Grid>))
-                }
-            </Grid>
-            <div ref={bottomRef}></div>
+            <InfiniteScroll ref={(scroll) => scroll}
+                dataLength={page * 20}
+                next={loadMore}
+                hasMore={true}
+                loader={
+                    <ThreeDots
+                        height="80"
+                        width="80"
+                        radius="9"
+                        color="#000"
+                        ariaLabel="three-dots-loading"
+                        wrapperStyle={{ justifyContent: 'center'}}
+                        wrapperClassName=""
+                        visible={true}
+                    />
+                }>
+                <Grid key={headline} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    {
+                        articles.map(article => (
+                            <Grid item xs={12} sm={6} md={3} key={article.publishedAt}>
+                                <MediaCard key={data.id} {...article}></MediaCard>
+                            </Grid>))
+                    }
+                </Grid>
+            </InfiniteScroll>
         </>
     )
 }
