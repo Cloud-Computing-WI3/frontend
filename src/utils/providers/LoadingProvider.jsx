4eb3c4952c57e3a875c72a9aacd8562af5a5411d
Kevin Eberhardt <eberhardt.kevin@outlook.com>
2023-01-13 11:35:03 +0100

    changed news feed provider service get categories

--- src/utils/providers/LoadingProvider.jsx
+++ src/utils/providers/LoadingProvider.jsx
@@ -3,8 +3,9 @@ export const LoadingContext = createContext(null);
 
 function LoadingProvider(props) {
     const [isLoading, setLoading] = useState(false);
+    const [loadingMessage, setLoadingMessage] = useState("");
     return (
-        <LoadingContext.Provider value={{isLoading, setLoading}}>
+        <LoadingContext.Provider value={{isLoading, setLoading, loadingMessage, setLoadingMessage}}>
             {props.children}
         </LoadingContext.Provider>
     );
